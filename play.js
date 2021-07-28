4b0edb1d3f4776b72afc92892e48afdc8f8f48ae
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-28 15:01:57 -0400

    Implemented the function

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
-const connect = require("./client.js");
 
-// setup interface to handle user input from stdin
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-setupInput();
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  };
-  if (key === 'up') {
-
-  };
-};
-stdin.on("data", handleUserInput);
 console.log("Connecting ...");
 
-connect();
+let conn = connect();
+setupInput(conn);
