9a6bf306378b9ade24ade965f984d40420e57e6f
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-28 13:54:16 -0400

    Export the function

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = function () {
+    const conn = net.createConnection({
+      host: '135.23.223.133', // IP address here,
+      port: 50542 // PORT number here,
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+    conn.on("connect", () => {
+        conn.write("Name: Ruchi")
+      // code that does something when the connection is first established
+      console.log("Connected!")
+    });
+    conn.on("data", (data) => {
+        console.log("data")
+        console.log(data)
+    })
+    return conn;
+  };
+
+module.exports = connect;
