f5346627a4b2e9ff8f0d537df065d43f8ab63c2c
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-28 16:01:13 -0400

    sending messages through keys

--- input.js
+++ input.js
@@ -14,12 +14,35 @@ const setupInput = function (conn) {
   
   const handleUserInput = function (key) {
     // your code here
+    process.stdout.write(`Hello ${key.toString()}`
+    ); 
+    if (key === 'w') {
+        connection.write("Move: up")
+    }
+    if (key === 'a') {
+        connection.write("Move: left")
+    }
+    if (key === 's') {
+        connection.write("Move: down")
+    }
+    if (key === 'd') {
+        connection.write("Move: right")
+    }
     if (key === '\u0003') {
       process.exit();
     };
-    if (key === 'up') {
-  
-    };
+    if (key === 'r') {
+        connection.write("Say: Hello There!")
+    }
+    if (key === 'u') {
+        connection.write("Say: Heyyy!")
+    }
+    if (key === 'c') {
+        connection.write("Say: Good Job!")
+    }
+    if (key === 'h') {
+        connection.write("Say: Connected!")
+    }
   };
   process.stdin.on("data", handleUserInput);
   module.exports = { setupInput };
