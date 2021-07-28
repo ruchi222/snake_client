4449b6f8e56a6d0a66e501666e2ac5f30d13a980
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-07-28 13:55:05 -0400

    implemented the function

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
+const connect = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: 50542 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Connected!")
-  });
-  conn.on("data", (data) => {
-      console.log("data")
-      console.log(data)
-  })
-  return conn;
-};
 
 console.log("Connecting ...");
 
